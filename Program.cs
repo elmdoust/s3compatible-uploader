667325c9cab6ceddb9719a60bb082a5ca3a84b98
BahramOnHamishehPC <hamisheh.development@gmail.com>
2022-10-11 14:49:12 +0330

    multipart uploading implemented

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using System.Reflection;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
+using Amazon.Runtime;
 
 namespace AutoBackupTool
 {
@@ -52,17 +54,24 @@ namespace AutoBackupTool
                                 var myFile = (from f in directory.GetFiles()
                                               orderby f.LastWriteTime descending
                                               select f).First();
-                                
                                 var path = $"{item.SourceDirectoryPath}/{myFile.Name}";
                                 Console.WriteLine("\n\n Please Wait, Object is uploading....... \n\n");
-                                await UploadObjectFromFileAsync(_s3Client, item.S3BucketName, myFile.Name, path);
+                                if (myFile.Length / 1024 / 1024 < 400)
+                                    await UploadObjectFromFileAsync(_s3Client, item.S3BucketName, myFile.Name, path);
+                                else
+                                    await UploadObjectAsync(_s3Client, item.S3BucketName, myFile.Name, path);
                                 break;
                             case "ALL_FILES":
-                                string ZipFileName = $"{item.SourceDirectoryPath.Replace("/", "-").Replace(":","")}-{DateTime.Now.ToString("yyyy-MM-dd-HH-mm")}.zip";
+                                string ZipFileName = $"{item.SourceDirectoryPath.Replace("/", "-").Replace(":", "")}-{DateTime.Now.ToString("yyyy-MM-dd-HH-mm")}.zip";
+                                Console.WriteLine("\n\n Zip object is creating....... \n\n");
                                 Helpers.ZipFiles(item.SourceDirectoryPath, ZipFileName);
-                                
+                                var myZipFileInfo = new FileInfo(ZipFileName);
                                 Console.WriteLine("\n\n Please Wait, Object is uploading....... \n\n");
-                                await UploadObjectFromFileAsync(_s3Client, item.S3BucketName, ZipFileName, ZipFileName);
+                                if (myZipFileInfo.Length / 1024 / 1024 < 400)
+                                    await UploadObjectFromFileAsync(_s3Client, item.S3BucketName, ZipFileName, ZipFileName);
+                                else
+                                    await UploadObjectAsync(_s3Client, item.S3BucketName, ZipFileName, ZipFileName);
+
                                 File.Delete(ZipFileName);
                                 break;
                         }
@@ -117,7 +126,98 @@ namespace AutoBackupTool
                 Helpers.writeLogs(DomainConfigs.LogFileName, ErrorMessage);
             }
         }
-        
+
+        public static async Task UploadObjectAsync(
+            IAmazonS3 client,
+            string bucketName,
+            string keyName,
+            string filePath)
+        {
+            // Create list to store upload part responses.
+            List<UploadPartResponse> uploadResponses = new List<UploadPartResponse>();
+
+            // Setup information required to initiate the multipart upload.
+            InitiateMultipartUploadRequest initiateRequest = new InitiateMultipartUploadRequest()
+            {
+                BucketName = bucketName,
+                Key = keyName,
+            };
+
+            // Initiate the upload.
+            InitiateMultipartUploadResponse initResponse =
+                await client.InitiateMultipartUploadAsync(initiateRequest);
+
+            // Upload parts.
+            long contentLength = new FileInfo(filePath).Length;
+            long partSize = 200 * (long)Math.Pow(2, 20); // 400 MB
+
+            try
+            {
+                Console.WriteLine("Uploading parts");
+
+                long filePosition = 0;
+                for (int i = 1; filePosition < contentLength; i++)
+                {
+                    UploadPartRequest uploadRequest = new UploadPartRequest()
+                    {
+                        BucketName = bucketName,
+                        Key = keyName,
+                        UploadId = initResponse.UploadId,
+                        PartNumber = i,
+                        PartSize = partSize,
+                        FilePosition = filePosition,
+                        FilePath = filePath,
+                    };
+
+                    // Track upload progress.
+                    uploadRequest.StreamTransferProgress +=
+                        new EventHandler<StreamTransferProgressArgs>(UploadPartProgressEventCallback);
+
+                    // Upload a part and add the response to our list.
+                    uploadResponses.Add(await client.UploadPartAsync(uploadRequest));
+
+                    filePosition += partSize;
+                }
+
+                // Setup to complete the upload.
+                CompleteMultipartUploadRequest completeRequest = new CompleteMultipartUploadRequest()
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    UploadId = initResponse.UploadId,
+                };
+                completeRequest.AddPartETags(uploadResponses);
+
+                // Complete the upload.
+                CompleteMultipartUploadResponse completeUploadResponse =
+                    await client.CompleteMultipartUploadAsync(completeRequest);
+                string FinalStatus = $"Object {keyName} added to {bucketName} bucket";
+                Console.WriteLine(FinalStatus);
+                Helpers.writeLogs(DomainConfigs.LogFileName, FinalStatus);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"An AmazonS3Exception was thrown: {exception.Message}");
+                string ErrorMessage = $"Error: {exception.Message}";
+                Console.WriteLine(ErrorMessage);
+                Helpers.writeLogs(DomainConfigs.LogFileName, ErrorMessage);
+
+                // Abort the upload.
+                AbortMultipartUploadRequest abortMPURequest = new AbortMultipartUploadRequest()
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    UploadId = initResponse.UploadId,
+                };
+                await client.AbortMultipartUploadAsync(abortMPURequest);
+            }
+        }
+
+        public static void UploadPartProgressEventCallback(object sender, StreamTransferProgressArgs e)
+        {
+            Console.WriteLine($"{e.TransferredBytes}/{e.TotalBytes}");
+        }
+
     }
 
 }
